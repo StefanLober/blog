912160f587e0dde9024109698dfaf645203a9b50
StefanLober <stefan.lober@outlook.de>
2020-07-22 18:44:45 +0200

    WPF test project initial

---
+++ WpfApplication07/GenericConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Linq;
+
+namespace Local
+{
+    public class GenericConverter<TKey, TValue> : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty ValuesProperty =
+            DependencyProperty.RegisterAttached("Values", typeof(Dictionary<TKey, TValue>), typeof(GenericConverter<TKey, TValue>));
+
+        public static void SetValues(DependencyObject obj, Dictionary<TKey, TValue> value)
+        {
+            obj.SetValue(ValuesProperty, value);
+        }
+
+        public static Dictionary<TKey, TValue> GetValues(DependencyObject obj)
+        {
+            return (Dictionary<TKey, TValue>)obj.GetValue(ValuesProperty);
+        }
+
+        public GenericConverter()
+        {
+            SetValues(this, new Dictionary<TKey, TValue>());
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var castedValue = (TKey)value;
+
+            if (targetType != typeof(TValue) || castedValue == null || !GetValues(this).ContainsKey(castedValue))
+                return default(TValue);
+
+            return GetValues(this)[(TKey)value];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var castedValue = (TValue)value;
+
+            if (targetType != typeof(TKey) || castedValue == null || !GetValues(this).ContainsValue(castedValue))
+                return null;
+
+            return GetValues(this).FirstOrDefault(x => x.Value.Equals(castedValue)).Key;
+        }
+    }
+
+    public class BrushConverter : GenericConverter<DisplayValueEnum, Brush> { }
+}
---
+++ WpfApplication07/ViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Local
+{
+    public class ActionCommand : ICommand
+    {
+        readonly Action _execute;
+
+        public ActionCommand(Action execute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+        }
+
+        public void Execute(object param) { _execute(); }
+
+        public bool CanExecute(object parameters) { return true; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        } 
+    }
+
+    public enum DisplayValueEnum : int
+    {
+        Undecided = 0,
+        Bad = 1,
+        Good = 2,
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+        
+        private ICommand _changeCommand;
+
+        public ICommand ChangeCommand
+        {
+            get
+            {
+                if (_changeCommand == null)
+                {
+                    _changeCommand = new ActionCommand(Change);
+                }
+
+                return _changeCommand;
+            }
+        }
+
+        private DisplayValueEnum _displayValue = DisplayValueEnum.Undecided;
+
+        public DisplayValueEnum DisplayValue
+        {
+            get { return _displayValue; }
+            set
+            {
+                if (value != _displayValue)
+                {
+                    _displayValue = value;
+                    OnPropertyChanged("DisplayValue");
+                }
+            }
+        }
+
+        private void Change() { DisplayValue = (DisplayValueEnum)(((int)DisplayValue + 1) % 3); }
+    }
+}
