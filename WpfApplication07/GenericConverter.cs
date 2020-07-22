a5f5dba056b61d829ed317560676cc946cde50f2
StefanLober <stefan.lober@outlook.de>
2020-07-22 18:55:12 +0200

    usings optimized

--- WpfApplication07/GenericConverter.cs
+++ WpfApplication07/GenericConverter.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
-using System.Linq;
 
 namespace Local
 {
